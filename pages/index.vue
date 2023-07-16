a3eb0da36d5ff63063ba358498a3d534f0f09632
ilyeezy <good.yoshi@yandex.ru>
2023-07-16 21:13:18 +0300

    initial calculator

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main">
+    <div class="main__wrapper wrapper">
+      <section class="wrapper__screen screen">0</section>
+      <section class="screen__calc-buttons">
+        <div class="screen__calc-button-row">
+          <button class="screen__calc-button-row__calc-button double">C</button>
+          <button class="screen__calc-button-row__calc-button">&larr;</button>
+          <button class="screen__calc-button-row__calc-button">&divide;</button>
+        </div>
+        <div class="screen__calc-button-row">
+          <button class="screen__calc-button-row__calc-button">7</button>
+          <button class="screen__calc-button-row__calc-button">8</button>
+          <button class="screen__calc-button-row__calc-button">9</button>
+          <button class="screen__calc-button-row__calc-button">&times;</button>
+        </div>
+        <div class="screen__calc-button-row">
+          <button class="screen__calc-button-row__calc-button">4</button>
+          <button class="screen__calc-button-row__calc-button">5</button>
+          <button class="screen__calc-button-row__calc-button">6</button>
+          <button class="screen__calc-button-row__calc-button">&minus;</button>
+        </div>
+        <div class="screen__calc-button-row">
+          <button class="screen__calc-button-row__calc-button">1</button>
+          <button class="screen__calc-button-row__calc-button">2</button>
+          <button class="screen__calc-button-row__calc-button">3</button>
+          <button class="screen__calc-button-row__calc-button">&plus;</button>
+        </div>
+        <div class="screen__calc-button-row">
+          <button class="screen__calc-button-row__calc-button null">0</button>
+          <button class="screen__calc-button-row__calc-button">,</button>
+          <button class="screen__calc-button-row__calc-button">&equals;</button>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
---
+++ plugins/index.js
@@ -0,0 +1,104 @@
+if (typeof document !== "undefined" && document) {
+  let runningTotal = 0;
+  let buffer = "0";
+  let previousOperator;
+  const screen = document.querySelector(".wrapper__screen");
+
+  function buttonClick(value) {
+    if (isNaN(value)) {
+      handleSymbol(value);
+    } else {
+      handleNumber(value);
+    }
+    screen.innerText = addThousandSeparators(buffer);
+  }
+  function addThousandSeparators(numberString) {
+    numberString = numberString.replace(/\s/g, "");
+    const parts = numberString.split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+
+    return parts.join(".");
+  }
+
+  function handleSymbol(symbol) {
+    switch (symbol) {
+      case "C":
+        buffer = "0";
+        runningTotal = 0;
+        break;
+      case "=":
+        if (previousOperator === null) {
+          return;
+        }
+        flushOperation(parseFloat(buffer));
+        previousOperator = null;
+        buffer = String(runningTotal);
+        runningTotal = 0;
+        break;
+      case "←":
+        if (buffer.length === 1) {
+          buffer = "0";
+        } else {
+          buffer = buffer.substring(0, buffer.length - 1);
+        }
+        break;
+      case ",":
+        if (!buffer.includes(".")) {
+          buffer += ".";
+        }
+        break;
+      case "+":
+      case "−":
+      case "×":
+      case "÷":
+        handleMath(symbol);
+        break;
+    }
+  }
+
+  function handleMath(symbol) {
+    if (buffer === "0") {
+      return;
+    }
+
+    const intBuffer = parseFloat(buffer);
+
+    if (runningTotal === 0) {
+      runningTotal = intBuffer;
+    } else {
+      flushOperation(intBuffer);
+    }
+    previousOperator = symbol;
+    buffer = "0";
+  }
+
+  function flushOperation(intBuffer) {
+    if (previousOperator === "+") {
+      runningTotal += parseFloat(intBuffer);
+    } else if (previousOperator === "−") {
+      runningTotal -= parseFloat(intBuffer);
+    } else if (previousOperator === "×") {
+      runningTotal *= parseFloat(intBuffer);
+    } else if (previousOperator === "÷") {
+      runningTotal /= parseFloat(intBuffer);
+    }
+  }
+
+  function handleNumber(numberString) {
+    if (buffer === "0") {
+      buffer = numberString;
+    } else {
+      buffer += numberString;
+    }
+  }
+
+  function init() {
+    document
+      .querySelector(".screen__calc-buttons")
+      .addEventListener("click", function (event) {
+        buttonClick(event.target.innerText);
+      });
+  }
+
+  init();
+}
